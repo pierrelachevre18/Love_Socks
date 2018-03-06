5ded63252b92800d336e748a463758af27d6781e
pierrelachevre18 <lpierre@stanford.edu>
2018-03-06 15:32:43 -0800

    up to date

--- Arduino Code/Love_Socks_AI/Love_Socks_AI.ino
+++ Arduino Code/Love_Socks_AI/Love_Socks_AI.ino
@@ -23,14 +23,14 @@
                                 // Good value as of 02-23 : 300
 #define POS_TAPE_THR     300    // Different to detect green tape too
 
-#define TALK_TIME_INTERVAL  2000000			//Interval between message display
+#define TALK_TIME_INTERVAL  2000000		//Interval between message display
 #define CTRL_INTERVAL       1000			//Interval between control value update
 #define POS_INTERVAL        1000			//Interval between position update
-#define SOLENOID_INTERVAL   500000    //Interval for solenoid 
+#define SOLENOID_INTERVAL   500000      //Interval for solenoid
 
 //Tape Follow
-#define PIN_RIGHT_SWIPER         A0
-#define PIN_LEFT_SWIPER        A1
+#define PIN_RIGHT_SWIPER        A0
+#define PIN_LEFT_SWIPER        	A1
 #define PIN_POS_SWIPER          A2
 //DC Motors
 #define PIN_PWM_RIGHT           A8
@@ -41,7 +41,7 @@
 #define PIN_IN_L_2              12
 
 //Buzzword Dispenser
-#define PIN_SOLENOID            7
+#define PIN_SOLENOID            6
 
 //Info LED
 #define PIN_RED_LED             3
@@ -50,8 +50,8 @@
 #define PIN_BEACON              A3
 
 //Nominal voltage for motors, 0<V<256 (needs room for controller though!)
-int V_nom_R=80;
-int V_nom_L=80;
+int V_nom_R=90;
+int V_nom_L=90;
 int V_nom_RT=65;        //For Turning
 
 //Controller parameters
@@ -63,6 +63,7 @@ int right_swiper = 0;
 int pos_swiper=0;
 int V_u=0;
 int dir_sign=1;
+int sol_open=1;
 
 /*---------------Module Function Prototypes-----------------*/
 
@@ -71,18 +72,18 @@ int dir_sign=1;
 // State for movement
 // Free is uncontrolled for testing
 typedef enum {
-  STATE_FWD, STATE_BCK, STATE_STOP, STATE_RT, STATE_FREE
+	STATE_FWD, STATE_BCK, STATE_STOP, STATE_RT, STATE_FREE
 } States_m;
 
 // State for positions
 typedef enum {
-  STATE_ONTAPE, STATE_OFFTAPE
+	STATE_ONTAPE, STATE_OFFTAPE
 } States_t;
 int pos_id=1;
 
 //State for global game playing
 typedef enum {
-  STATE_PHASE1, STATE_PHASE2, STATE_PHASE3
+	STATE_PHASE1, STATE_PHASE2, STATE_PHASE3
 } States_g;
 
 /*---------------Module Variables---------------------------*/
@@ -92,17 +93,18 @@ States_g state_g;
 IntervalTimer dispTimer;
 IntervalTimer tapeTimer;
 IntervalTimer posTimer;
-static Metro solTimer = Metro(SOLENOID_INTERVAL);
-
+IntervalTimer solTimer;
 
 /*---------------Main Functions----------------*/
 
 void setup() {
   //Initiate Timers
-  Serial.begin(9600);
-  //dispTimer.begin(say_stuff,TALK_TIME_INTERVAL);
+	Serial.begin(9600);
+  dispTimer.begin(say_stuff,TALK_TIME_INTERVAL);
   tapeTimer.begin(tapeController,CTRL_INTERVAL);    //Needs to be updated at constant intervals
-  posTimer.begin(updatePos,POS_INTERVAL);
+  posTimer.begin(updatePos,POS_INTERVAL); 
+  //solTimer.begin(updateSol,SOLENOID_INTERVAL);
+  
 
   //Initiate States
   state_m = STATE_FWD;
@@ -118,14 +120,17 @@ void setup() {
   pinMode(PIN_IN_R_2, OUTPUT);
   pinMode(PIN_SOLENOID,OUTPUT);
   pinMode(PIN_RED_LED,OUTPUT);
+
+  //Initiate Solenoid
+  closeSolenoid();
 }
 
 void loop() {
-  left_swiper = analogRead(PIN_LEFT_SWIPER);
-  right_swiper = analogRead(PIN_RIGHT_SWIPER);
-  pos_swiper=analogRead(PIN_POS_SWIPER);
-  handleMove();
-  checkGlobalEvents();
+	left_swiper = analogRead(PIN_LEFT_SWIPER);
+	right_swiper = analogRead(PIN_RIGHT_SWIPER);
+	pos_swiper=analogRead(PIN_POS_SWIPER);
+	handleMove();
+	checkGlobalEvents();
 }
 
 /*----------------Core Functions--------------------------*/
@@ -133,148 +138,163 @@ void loop() {
 void say_stuff()
 {
   //Useful do display stuff to debug
-  Serial.println("Swipers");
-  Serial.println(left_swiper);
-  Serial.println(right_swiper);
-  Serial.println(pos_swiper);
-  Serial.println("Pos");
-  Serial.println(pos_id);
-  Serial.println(state_t);
+	//Serial.println("Swipers");
+	//Serial.println(left_swiper);
+	//Serial.println(right_swiper);
+	//Serial.println(pos_swiper);
+	//Serial.println("Pos");
+	//Serial.println(pos_id);
+	//Serial.println(state_t);
 }
 void checkGlobalEvents(void) {
   //check for events
 	if((state_g==STATE_PHASE2) && (pos_id>=10)) {
-	state_m=STATE_RT;
+		state_m=STATE_RT;
   //redLEDOn();
 	}
 	if(state_g==STATE_PHASE2 && state_m==STATE_RT && right_swiper<TAPE_THR) {
-	state_g=STATE_PHASE3;
+		state_g=STATE_PHASE3;
 	}
-  if(state_g==STATE_PHASE3 && right_swiper>TAPE_THR) {
-  state_m=STATE_FWD;
-  }
-  if (solTimer.check()){
-    closeSolenoid();
-  }
+	if(state_g==STATE_PHASE3 && right_swiper>TAPE_THR) {
+		state_m=STATE_FWD;
+	}
 }
 
 void redLEDOn(void) {
-  digitalWrite(PIN_RED_LED,HIGH);
+	digitalWrite(PIN_RED_LED,HIGH);
 }
 
 void redLEDOff(void)
 {
-  digitalWrite(PIN_RED_LED,LOW);
+	digitalWrite(PIN_RED_LED,LOW);
 }
 
 /*----------Movement Functions-----------------*/
 
 void handleMove(void) {
-  switch (state_m) {
-    case STATE_FWD:
-    rightFwd(V_nom_R+V_u);
-    leftFwd(V_nom_L-V_u);
-    break;
-    case STATE_BCK:
-    rightBck(V_nom_R-V_u);
-    leftBck(V_nom_L+V_u);
-    break;
-    case STATE_STOP:
-    rightOff();
-    leftOff();
-    break;
-    case STATE_FREE:
-    rightFwd(V_nom_R);
-    leftFwd(V_nom_L);
-    break;
-    case STATE_RT:
-    rightBck(V_nom_RT);
-    leftFwd(V_nom_RT);
-    break;
+	switch (state_m) {
+		case STATE_FWD:
+		rightFwd(V_nom_R+V_u);
+		leftFwd(V_nom_L-V_u);
+		break;
+		case STATE_BCK:
+		rightBck(V_nom_R);
+		leftBck(V_nom_L);
+		break;
+		case STATE_STOP:
+		rightOff();
+		leftOff();
+		break;
+		case STATE_FREE:
+		rightFwd(V_nom_R);
+		leftFwd(V_nom_L);
+		break;
+		case STATE_RT:
+		//rightBck(V_nom_RT);
+		//leftFwd(V_nom_RT);
+    rightBck(2*V_nom_RT);
+    leftBck(V_nom_RT);
+		break;
       default:    // Should never get into an unhandled state
       Serial.println("Unplanned Motor State");
-    }
   }
+}
 
-  void tapeController(void) {
+void tapeController(void) {
   // Controller for line follow, discrete version
   // Needs to return an int at most 256-V_nom
-    V_u=Kp*((right_swiper>TAPE_THR)-(left_swiper>TAPE_THR));
-  }
+	V_u=Kp*((right_swiper>TAPE_THR)-(left_swiper>TAPE_THR));
+}
 
 // Individual motor functions
 
-  void leftFwd(int speed){
-    digitalWrite(PIN_IN_L_1,LOW);
-    digitalWrite(PIN_IN_L_2,HIGH);
-    analogWrite(PIN_PWM_LEFT,speed);
-  }
+void leftFwd(int speed){
+	digitalWrite(PIN_IN_L_1,LOW);
+	digitalWrite(PIN_IN_L_2,HIGH);
+	analogWrite(PIN_PWM_LEFT,speed);
+}
 
-  void leftBck(int speed){
-    digitalWrite(PIN_IN_L_1,HIGH);
-    digitalWrite(PIN_IN_L_2,LOW);
-    analogWrite(PIN_PWM_LEFT,speed);
-  }
+void leftBck(int speed){
+	digitalWrite(PIN_IN_L_1,HIGH);
+	digitalWrite(PIN_IN_L_2,LOW);
+	analogWrite(PIN_PWM_LEFT,speed);
+}
 
-  void leftOff(void){
-    digitalWrite(PIN_IN_L_1,LOW);
-    digitalWrite(PIN_IN_L_2,LOW);
-  }
+void leftOff(void){
+	digitalWrite(PIN_IN_L_1,LOW);
+	digitalWrite(PIN_IN_L_2,LOW);
+}
 
-  void rightFwd(int speed){
-    digitalWrite(PIN_IN_R_1,LOW);
-    digitalWrite(PIN_IN_R_2,HIGH);
-    analogWrite(PIN_PWM_RIGHT,speed);
-  }
+void rightFwd(int speed){
+	digitalWrite(PIN_IN_R_1,LOW);
+	digitalWrite(PIN_IN_R_2,HIGH);
+	analogWrite(PIN_PWM_RIGHT,speed);
+}
 
-  void rightBck(int speed){
-    digitalWrite(PIN_IN_R_1,HIGH);
-    digitalWrite(PIN_IN_R_2,LOW);
-    analogWrite(PIN_PWM_RIGHT,speed);
-  }
+void rightBck(int speed){
+	digitalWrite(PIN_IN_R_1,HIGH);
+	digitalWrite(PIN_IN_R_2,LOW);
+	analogWrite(PIN_PWM_RIGHT,speed);
+}
 
-  void rightOff(void){
-    digitalWrite(PIN_IN_R_1,LOW);
-    digitalWrite(PIN_IN_R_2,LOW);
-  }
+void rightOff(void){
+	digitalWrite(PIN_IN_R_1,LOW);
+	digitalWrite(PIN_IN_R_2,LOW);
+}
 
 /*-------Position Functions--------*/
 
-  void updatePos(void){
+void updatePos(void){
   //Increment position when needed
-    switch (state_t) {
-      case STATE_ONTAPE:
-      if (pos_swiper>POS_TAPE_THR*1.05) {
-        state_t=STATE_OFFTAPE;
-        pos_id=pos_id+(dir_sign+1)/2;
-        Serial.println("Line passed");
-        Serial.println(pos_swiper);
-        Serial.println(pos_id);
-        redLEDOff();
-        openSolenoid();
-        solTimer.reset();
-      }
-      break;
-      case STATE_OFFTAPE:
-      if (pos_swiper<POS_TAPE_THR*0.95){
-        state_t=STATE_ONTAPE;
-        pos_id=pos_id+(dir_sign+1)/2;
-        Serial.println("Line reached");
-        Serial.println(pos_swiper);
-        Serial.println(pos_id);
-        redLEDOn();
-      }
-      break;
-      default:
-      Serial.println("Unplanned Position State");
-    }
-  }
+	switch (state_t) {
+		case STATE_ONTAPE:
+		if (pos_swiper>POS_TAPE_THR*1.05) {
+			state_t=STATE_OFFTAPE;
+			pos_id=pos_id+(dir_sign+1)/2;
+			Serial.println("Line passed");
+			Serial.println(pos_swiper);
+			Serial.println(pos_id);
+			redLEDOff();
+      closeSolenoid(); 
+		}
+		break;
+		case STATE_OFFTAPE:
+		if (pos_swiper<POS_TAPE_THR*0.95){
+			state_t=STATE_ONTAPE;
+			pos_id=pos_id+(dir_sign+1)/2;
+			Serial.println("Line reached");
+			Serial.println(pos_swiper);
+			Serial.println(pos_id);
+			redLEDOn();
+      openSolenoid();
+		}
+		break;
+		default:
+		Serial.println("Unplanned Position State");
+	}
+}
 
 /*---------Buzzword Functions---------------*/
-  void openSolenoid(void){
-    digitalWrite(PIN_SOLENOID, HIGH);
-  }
+void openSolenoid(void){
+	digitalWrite(PIN_SOLENOID, HIGH);
+}
 
-  void closeSolenoid(void){
-    digitalWrite(PIN_SOLENOID, LOW);
+void closeSolenoid(void){
+	digitalWrite(PIN_SOLENOID, LOW);
+}
+
+void updateSol(void){
+  switch (sol_open){
+    case 1:
+      closeSolenoid();
+      Serial.println("Closing");
+      sol_open=0;
+    case 0:
+      if (pos_id==4) {
+        openSolenoid();
+        Serial.println("Opening");
+        sol_open=1;
+      }
   }
+}
+
